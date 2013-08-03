94e8731872b864a20a03e759a8a4fba5f2372107
David Williams <dpw341@gmail.com>
2013-08-03 14:55:28 +0100

    Added test command for py.test to setup.py and updated .gitignore

--- setup.py
+++ setup.py
@@ -1,6 +1,24 @@
 from distutils.core import setup
+from setuptools.command.test import test as TestCommand
+import sys
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_suite = True
+        self.test_args = []
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
 
 setup(name='cljitertools',
       version='0.1',
       py_modules=['cljitertools'],
+      tests_require=['pytest==2.3.5'],
+      cmdclass = {'test': PyTest}
       )
