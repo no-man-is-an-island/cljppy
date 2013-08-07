bdea47ccc71de02499c9a1d2364c3df8553bfd10
David Williams <david.williams@david-HP.solocapital.local>
2013-08-07 14:21:39 +0100

    some performance optimisations on random access time in lazy sequences

--- lazysequence.py
+++ lazysequence.py
@@ -14,6 +14,9 @@ class LazySequenceIterator(object):
         except IndexError:
             raise StopIteration
 
+    def reset(self):
+        self.__index = 0
+
     def __iter__(self):
         return self
 
@@ -26,30 +29,30 @@ class LazySequence(object):
         """
         self.realised = False
         self.__realised_segment = []
+        self.__realised_segment_size = 0
         self.__source_it = iter(source)
         self.print_length = printlength
         try:
             self.__realised_segment.append(self.__source_it.next())
+            self.__realised_segment_size += 1
         except StopIteration:
             self.realised = True
 
-    def __getitem__(self, item):
-        if type(item) is slice:
-            s = []
-            for idx in range(item.start, item.stop):
-                s.append(self[idx])
-            return s
+    def __getslice__(self, start, stop):
+        s = []
+        for idx in range(start, stop):
+            s.append(self[idx])
+        return s
 
-        if item >= len(self.__realised_segment):
+    def __getitem__(self, item):
+        if item >= self.__realised_segment_size:
             if self.realised:
                 raise IndexError("index out of range")
-            while (not self.realised) and len(self.__realised_segment) != item + 1:
+            while (not self.realised) and self.__realised_segment_size != item + 1:
                 self.realise_next()
             return self[item]
-        else:
-            if item == len(self.__realised_segment):
-                raise IndexError("index out of range")
-            return self.__realised_segment[item]
+
+        return self.__realised_segment[item]
 
     def __len__(self):
         if not self.realised:
@@ -63,7 +66,7 @@ class LazySequence(object):
         return self.__repr__()
 
     def __repr__(self):
-        if self.realised and self.print_length >= len(self.__realised_segment):
+        if self.realised and self.print_length >= self.__realised_segment_size:
             return str(self.__realised_segment)
         return "[" + ', '.join(take(self.print_length,
                                     imap(str, self.__realised_segment))) + ", ...]"
@@ -88,6 +91,7 @@ class LazySequence(object):
     def realise_next(self):
         try:
             self.__realised_segment.append(self.__source_it.next())
+            self.__realised_segment_size += 1
         except StopIteration:
             self.realised = True
 
@@ -95,4 +99,5 @@ class LazySequence(object):
         if not self.realised:
             for x in self.__source_it:
                 self.__realised_segment.append(x)
+                self.__realised_segment_size += 1
             self.realised = True
