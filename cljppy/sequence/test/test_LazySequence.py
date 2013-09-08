b4b07ad0d7c0c559cac0f0ab47fe8e137002f208
David Williams <dpw341@gmail.com>
2013-09-08 12:20:11 +0100

    Slightly better tests for hashing of lazyseqs

--- cljppy/sequence/test/test_LazySequence.py
+++ cljppy/sequence/test/test_LazySequence.py
@@ -1,5 +1,7 @@
+from cljppy import natural_numbers, take
 from cljppy.sequence.LazySequence import LazySequence
 
 
 def test_hash_codes_of_lazysequences_make_sense():
-    assert hash(LazySequence([1,2,3])) == hash(LazySequence([1,2,3]))
+    assert hash(LazySequence([0, 1, 2])) == hash(LazySequence(take(3, natural_numbers())))
+    assert hash(LazySequence([0, 1, 2, 3])) != hash(LazySequence(take(3, natural_numbers())))
