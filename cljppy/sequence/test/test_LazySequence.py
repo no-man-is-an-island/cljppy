d44ae9ea154f5c2411d5f0956970c8d7ebfbbd49
David Williams <dpw341@gmail.com>
2013-09-08 12:45:47 +0100

    Lazy sequences are lazy

--- cljppy/sequence/test/test_LazySequence.py
+++ cljppy/sequence/test/test_LazySequence.py
@@ -5,3 +5,9 @@ from cljppy.sequence.LazySequence import LazySequence
 def test_hash_codes_of_lazysequences_make_sense():
     assert hash(LazySequence([0, 1, 2])) == hash(LazySequence(take(3, natural_numbers())))
     assert hash(LazySequence([0, 1, 2, 3])) != hash(LazySequence(take(3, natural_numbers())))
+
+
+def test_lazy_sequences_are_lazy():
+    n = natural_numbers()
+    assert take(5, n) == [0, 1, 2, 3, 4]
+    assert not n.realised
